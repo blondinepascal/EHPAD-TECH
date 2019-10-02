7e1bec87b374177e726be44a33d6b6fd10f9dfe5
FOUENANG Blondine <blondine-pascale.fouenang-kenfack@etu.u-pec.fr>
2019-10-02 05:17:29 +0200

    Alert tools

---
+++ EHPADTECH-master/commons/src/main/java/com/ehpadtech/monitor/commons/entity/Sensitivity.java
@@ -0,0 +1,9 @@
+package com.ehpadtech.monitor.commons.entity;
+
+
+public enum Sensitivity {
+	/**
+	 * Different sensitivity of an sensor, they have 3 level of sensitivity
+	 */
+	LOW, MEDIUM, HIGH
+}
