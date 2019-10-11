05abc8e41102ccc55cc4d585142e581dab9a8bcc
pasca <pasca@10.10.25.141>
2019-10-11 06:38:26 +0200

    sensor type

--- EHPADTECH-master/commons/src/main/java/com/ehpadtech/monitor/commons/entity/SensorType.java
+++ EHPADTECH-master/commons/src/main/java/com/ehpadtech/monitor/commons/entity/SensorType.java
@@ -5,13 +5,5 @@ public enum SensorType {
 	/**
 	 * The different sensors existing in the system
 	 */
-	SMOKE, MOVE, TEMPERATURE, WINDOW, DOOR, ELEVATOR, LIGHT, FIRE,package com.ehpadtech.monitor.commons.entity;
-
-
-public enum SensorType {
-	/**
-	 * The different sensors existing in the system
-	 */
-	SMOKE, MOVE, TEMPERATURE, WINDOW, DOOR, ELEVATOR, LIGHT, FIRE
-}
+	SMOKE, MOVE, TEMPERATURE
 }
