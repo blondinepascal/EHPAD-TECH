5f6a7c0e883a5492807e257255c2e9644ddd5a9b
Blondine Fouenang <pascalefouenang@gmail.com>
2019-12-06 15:51:18 +0100

    update server according to the VM interface without JFrame

--- EHPADTECH-master/connectionpool/src/main/java/com/ehpadtech/monitor/connection/launcher/MainServerGUI.java
+++ EHPADTECH-master/connectionpool/src/main/java/com/ehpadtech/monitor/connection/launcher/MainServerGUI.java
@@ -1,22 +1,15 @@
 package com.ehpadtech.monitor.connection.launcher;
 
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.util.ResourceBundle;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.ehpadtech.monitor.connection.socket.Server;
 
 
-public class MainServerGUI extends JFrame {
+public class MainServerGUI  {
 
 	/**
 	 * Initialization of different parameters
@@ -25,17 +18,9 @@ public class MainServerGUI extends JFrame {
 	private String host;
 	private int port;
 	private static int nbServer = 0;
-	private int hour = 0;
-	private int minute = 0;
-	private int seconde = 0;
-	private ActionListener tache_timer;
+
 	private Server ts;
-	private JLabel time;
-	private JPanel container;
-	private JButton launch;
-	private JButton launchFake;
-	private JButton stop;
-	private Timer timer1;
+	
 	private static final Logger logger = LogManager.getLogger(MainServerGUI.class);
 
 	/**
@@ -46,102 +31,21 @@ public class MainServerGUI extends JFrame {
 		ResourceBundle rs = ResourceBundle.getBundle("config");
 		this.host = rs.getString("server.host");
 		this.port = Integer.parseInt(rs.getString("server.port"));
-
-		time = new JLabel();
-		container = new JPanel();
-		container.setLayout(new GridLayout(2, 2));
-		/**
-		 * Create a button to launch the server
-		 */
-		ts = new Server(host, port);
-
+		ts = new Server(host, port);	
 		
-
-		/**
-		 * Button who invoke the normal method who give one connection to one socket
-		 */
-		launch = new JButton("Start server");
-		launch.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
 				ts.load();
 				if (nbServer == 0) {
 
 					ts.open();
 					nbServer++;
 					logger.log(Level.INFO, "Server Initialized");
-
-					/**
-					 * Creation of Timer to know how many time the server was launch
-					 */
-					int delais = 1000;
-					tache_timer = new ActionListener() {
-						public void actionPerformed(ActionEvent e1) {
-							seconde++;
-							if (seconde == 60) {
-								seconde = 0;
-								minute++;
-							}
-							if (minute == 60) {
-								minute = 0;
-								hour++;
-							}
-							time.setText(hour + ":" + minute + ":" + seconde);
-						}
-					};
-
-					timer1 = new Timer(delais, tache_timer);
-					timer1.start();
+					System.out.println("server start");
 					ts.treatment();
 				} else {
-					logger.log(Level.INFO, "Server already launch");
+					logger.log(Level.INFO, "Server not Initialized");
 				}
 			}
-		});
-
-		/**
-		 * Button to invoke the method who create a socket accept who block all the
-		 * connection but accept the socket
-		 */
 		
-
-		/**
-		 * Create a button to stop the server
-		 */
-		stop = new JButton("Stop server");
-		stop.addActionListener(new ActionListener() {
-			/**
-			 * Stop the server, stop the timer and set the number to server launch to 0
-			 */
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (nbServer > 0) {
-					ts.close();
-					timer1.stop();
-					nbServer = 0;
-					logger.log(Level.INFO, "Server Stoped");
-					logger.log(Level.INFO, "Application closed");
-				}
-			}
-		});
-
-		/**
-		 * Add the buttons to container
-		 */
-		container.add(launch);
-		container.add(stop);
-		container.add(time);
-		container.setBackground(Color.PINK);
-
-		/**
-		 * Define the frame
-		 */
-		this.setContentPane(container);
-		this.setTitle("Server");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setSize(300, 150);
-	}
 
 	/**
 	 * Main to open the Server
@@ -150,6 +54,6 @@ public class MainServerGUI extends JFrame {
 	 */
 	public static void main(String[] args) {
 		MainServerGUI frame = new MainServerGUI();
-		frame.setVisible(true);
+		
 	}
 }
